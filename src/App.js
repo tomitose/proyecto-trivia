3f8f8286c944022b47b1144004a89f025991a0db
tomitose <tomitosello@gmail.com>
2023-02-14 10:46:25 -0300

    pene de mono

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.jsaa</code> 
+          Edit <code></code> 
+          Pene de mono
         </p>
 
       </header>
