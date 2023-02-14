1f3aa999c8e03a4e509c847dd78ef055696d79e5
Matias Asenjo Pino <matia.cba74@gmail.com>
2023-02-14 11:44:16 -0300

    Circle Finished

--- src/components/Ruleta/Ruleta.jsx
+++ src/components/Ruleta/Ruleta.jsx
@@ -1,8 +1,71 @@
 import React from 'react'
-
+import "./Ruleta.css"
 const Ruleta = () => {
   return (
-    <div>Ruleta</div>
+    <div>
+    <ul class="circle">
+        <li>
+            <div class="text"
+            contenteditable="true"
+            spellcheck="false">1</div>
+        </li>
+        <li>
+            <div class="text"
+            contenteditable="true"
+            spellcheck="false">2</div>
+        </li>
+        <li>
+            <div class="text"
+            contenteditable="true"
+            spellcheck="false">3</div>
+        </li>
+        <li>
+            <div class="text"
+            contenteditable="true"
+            spellcheck="false">4</div>
+        </li>
+        <li>
+            <div class="text"
+            contenteditable="true"
+            spellcheck="false">5</div>
+        </li>
+        <li>
+            <div class="text"
+            contenteditable="true"
+            spellcheck="false">6</div>
+        </li>
+        <li>
+            <div class="text"
+            contenteditable="true"
+            spellcheck="false">7</div>
+        </li>
+        <li>
+            <div class="text"
+            contenteditable="true"
+            spellcheck="false">8</div>
+        </li>
+        <li>
+            <div class="text"
+            contenteditable="true"
+            spellcheck="false">9</div>
+        </li>
+        <li>
+            <div class="text"
+            contenteditable="true"
+            spellcheck="false">10</div>
+        </li>
+        <li>
+            <div class="text"
+            contenteditable="true"
+            spellcheck="false">11</div>
+        </li>
+        <li>
+            <div class="text"
+            contenteditable="true"
+            spellcheck="false">12</div>
+        </li>
+    </ul>
+</div>
   )
 }
 
