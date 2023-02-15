c38c4a3f5b3731808190c085ffa30d651a10e9eb
tomitose <tomitosello@gmail.com>
2023-02-15 15:39:21 -0300

    Colores y font general

--- src/components/Memo/Card/Card.jsx
+++ src/components/Memo/Card/Card.jsx
@@ -1,14 +1,14 @@
-import React from 'react'
-import "../Memo.css"
+import React from "react";
+import "./Card.css";
 
-const Card = ({ item ,id, handleClick}) => {
-    const intemClass = item.stat ? "active " + item.stat: ""
+const Card = ({ item, id, handleClick }) => {
+  const itemClass = item.stat ? "active " + item.stat : "";
 
   return (
-    <div className={"card" + intemClass} onClick={()=> handleClick(id)}>
-        <img className='img-card' src={item.img} alt="img" />
+    <div className={"card" + itemClass} onClick={() => handleClick(id)}>
+      <img className="img-card" src={item.img} alt="img" />
     </div>
-  )
-}
+  );
+};
 
-export default Card
+export default Card;
--- src/components/Memo/MemoCards/MemoCards.jsx
+++ src/components/Memo/MemoCards/MemoCards.jsx
@@ -20,42 +20,16 @@ const MemoCards = () => {
       { id: 7, img: "/img/memoImagenes/angular.png", stat: "" },
       { id: 7, img: "/img/memoImagenes/angular.png", stat: "" },
       { id: 8, img: "/img/memoImagenes/nodejs.png", stat: "" },
-      { id: 8, img: "/img/memoImagenes/nodejs.png", stat: "" },
+      { id: 8, img: "/img/memoImagenes/nodejs.png", stat: "" }
     ].sort(() => Math.random() - 0.5)
   );
 
-  const [prev, setPrev] = useState(-1);
 
-  function check(current) {
-    if (items[current].id === items[prev].id) {
-      items[current].stat = "correct";
-      items[prev].stat = "correct";
-      setItems([...items])
-      setPrev(-1);
-    }else{
-        items[current].stat = "wrong"
-        items[prev].stat = "wrong"
-        setItems([...items])
-        setTimeout(() => {
-            items[current].stat = ""
-            items[prev].stat = ""
-            setItems([...items])
-            setPrev(-1);
-        }, 1000);
-    }
-  };
 
   function handleClick(id){
-    if (prev === -1) {
-      items[id].stat = "active";
-      setItems([...items]);
-      setPrev(id);
-    } else {
-      check(id);
-    }
-
-    items[id].stat = "active";
+ 
     setItems([...items]);
+    alert(id)
 };
 
   return (
