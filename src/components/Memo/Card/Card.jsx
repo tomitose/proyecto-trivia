deeda9ac574cd676f231cc38f3b79ec6d0fde45d
tomitose <tomitosello@gmail.com>
2023-02-15 09:19:33 -0300

    Modificaciones en Memo generales

--- src/components/Memo/Card/Card.jsx
+++ src/components/Memo/Card/Card.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
-import "./Card.css"
+import "../Memo.css"
 
-const Card = ({ item }) => {
+const Card = ({ item ,id, handleClick}) => {
+    const intemClass = item.stat ? "active " + item.stat: ""
+
   return (
-    <div className='card-container'>
+    <div className={"card" + intemClass} onClick={()=> handleClick(id)}>
         <img className='img-card' src={item.img} alt="img" />
     </div>
   )
--- src/components/Memo/MemoCards/MemoCards.jsx
+++ src/components/Memo/MemoCards/MemoCards.jsx
@@ -3,31 +3,69 @@ import Card from "../Card/Card";
 import "./MemoCards.css";
 
 const MemoCards = () => {
-  const [items, setItems] = useState([
-    { id: 1, img: "/img/memoImagenes/html.png", stat: "" },
-    { id: 1, img: "/img/memoImagenes/html.png", stat: "" },
-    { id: 2, img: "/img/memoImagenes/css.png", stat: "" },
-    { id: 2, img: "/img/memoImagenes/css.png", stat: "" },
-    { id: 3, img: "/img/memoImagenes/js.png", stat: "" },
-    { id: 3, img: "/img/memoImagenes/js.png", stat: "" },
-    { id: 4, img: "/img/memoImagenes/scss.png", stat: "" },
-    { id: 4, img: "/img/memoImagenes/scss.png", stat: "" },
-    { id: 5, img: "/img/memoImagenes/react.png", stat: "" },
-    { id: 5, img: "/img/memoImagenes/react.png", stat: "" },
-    { id: 6, img: "/img/memoImagenes/vue.png", stat: "" },
-    { id: 6, img: "/img/memoImagenes/vue.png", stat: "" },
-    { id: 7, img: "/img/memoImagenes/angular.png", stat: "" },
-    { id: 7, img: "/img/memoImagenes/angular.png", stat: "" },
-    { id: 8, img: "/img/memoImagenes/nodejs.png", stat: "" },
-    { id: 8, img: "/img/memoImagenes/nodejs.png", stat: "" },
-  ]);
+  const [items, setItems] = useState(
+    [
+      { id: 1, img: "/img/memoImagenes/html.png", stat: "" },
+      { id: 1, img: "/img/memoImagenes/html.png", stat: "" },
+      { id: 2, img: "/img/memoImagenes/css.png", stat: "" },
+      { id: 2, img: "/img/memoImagenes/css.png", stat: "" },
+      { id: 3, img: "/img/memoImagenes/js.png", stat: "" },
+      { id: 3, img: "/img/memoImagenes/js.png", stat: "" },
+      { id: 4, img: "/img/memoImagenes/scss.png", stat: "" },
+      { id: 4, img: "/img/memoImagenes/scss.png", stat: "" },
+      { id: 5, img: "/img/memoImagenes/react.png", stat: "" },
+      { id: 5, img: "/img/memoImagenes/react.png", stat: "" },
+      { id: 6, img: "/img/memoImagenes/vue.png", stat: "" },
+      { id: 6, img: "/img/memoImagenes/vue.png", stat: "" },
+      { id: 7, img: "/img/memoImagenes/angular.png", stat: "" },
+      { id: 7, img: "/img/memoImagenes/angular.png", stat: "" },
+      { id: 8, img: "/img/memoImagenes/nodejs.png", stat: "" },
+      { id: 8, img: "/img/memoImagenes/nodejs.png", stat: "" },
+    ].sort(() => Math.random() - 0.5)
+  );
+
+  const [prev, setPrev] = useState(-1);
+
+  function check(current) {
+    if (items[current].id === items[prev].id) {
+      items[current].stat = "correct";
+      items[prev].stat = "correct";
+      setItems([...items])
+      setPrev(-1);
+    }else{
+        items[current].stat = "wrong"
+        items[prev].stat = "wrong"
+        setItems([...items])
+        setTimeout(() => {
+            items[current].stat = ""
+            items[prev].stat = ""
+            setItems([...items])
+            setPrev(-1);
+        }, 1000);
+    }
+  };
+
+  function handleClick(id){
+    if (prev === -1) {
+      items[id].stat = "active";
+      setItems([...items]);
+      setPrev(id);
+    } else {
+      check(id);
+    }
+
+    items[id].stat = "active";
+    setItems([...items]);
+};
 
   return (
-      <div className="container-cards">
-        {items.map((item,index)=>{
-           return <Card key={index} item={item}/>
-        })}
-      </div>
+    <div className="container-cards">
+      {items.map((item, index) => {
+         return(
+          <Card key={index} item={item} id={item.id} handleClick={handleClick} />
+        );
+      })}
+    </div>
   );
 };
 
