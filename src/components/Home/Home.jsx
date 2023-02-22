15111d8609335373807915e07da8c27b2964897b
Matias Asenjo Pino <matia.cba74@gmail.com>
2023-02-22 09:21:28 -0300

    Commit 22/2

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,8 +1,20 @@
+import { Button } from '@mui/material'
 import React from 'react'
+import { useNavigate } from 'react-router'
+import "./Home.css"
 
 const Home = () => {
+
+  const navigate = useNavigate()
+
   return (
-    <div>Home</div>
+    <div style={{height:'100%',width:'100%',display:'flex',justifyContent:'center',alignItems:'center'}}>
+      <div style={{height:'50%',width:'50%',display:'flex',flexDirection:'column',alignItems:'center',justifyContent:'space-evenly'}}>
+        <Button className="btn-home" variant="contained" onClick={() => navigate('/ruleta')}>Ruleta</Button>
+        <Button className="btn-home" variant="contained" onClick={() => navigate('/memo')}>Memo</Button>
+        <Button className="btn-home" variant="contained" onClick={() => navigate('/otro')}>Otro</Button>
+      </div>
+    </div>
   )
 }
 
--- src/components/Ruleta/Ruleta.jsx
+++ src/components/Ruleta/Ruleta.jsx
@@ -1,18 +1,20 @@
-import React, { useState} from 'react'
+import { duration, outlinedInputClasses } from '@mui/material';
+import React, { useState } from 'react'
 import ModalRuleta from './Modal/ModalRuleta';
 import "./Ruleta.css"
 
+
+//Dejar parcheado 
 
 const Ruleta = () => {
 
     const [frase, setFrase] = useState({ name: "circle" }); //Buscar reset despues de cada blick en boton
     const [dataForModal, setDataForModal] = useState({ id: 0, tematica: "", image: "" });
     const [openModalState, setOpenModalState] = useState(false);
-    const [disabledButton, setDisabledButton] = useState(false);
+    const [disabledButton, setDisabledButton] = useState(true);
 
 
     const closeModal = () => {
-        setDisabledButton(false)
         setOpenModalState(false)
         setFrase({
             name: "circle"
@@ -20,14 +22,14 @@ const Ruleta = () => {
     }
 
     const data = [
-        { id: 1, tematica: "Juicio Juntas", image: "/img/ruletaImagenes/tematica1.jpg", opt1: "Juicio Juntas", opt2: "Diputados de la Nacion recibiendo distinción", opt3: "Juicio por las Malvinas" },
-        { id: 2, tematica: "Leyes Obedencia debida y punto", image: "/img/ruletaImagenes/tematica2.jpg", opt1: "Leyes Obedencia debida y punto", opt2: "Menem asume la presidencia", opt3: "Oficiales Armadas presentandose ante la nación" },
-        { id: 3, tematica: "Vuelta a la democracia", image: "/img/ruletaImagenes/tematica3.jpg", opt1: "Vuelta a la democracia", opt2: "Creacion de la nueva bandera Argentina", opt3: "Presidente de Estados Unidos visita la casa Rosada" },
-        { id: 4, tematica: "Nestor Kirchner baja los cuadros de la dictadura", image: "/img/ruletaImagenes/tematica4.jpg", opt1: "Nestor Kirchner baja los cuadros de la dictadura", opt2: "Nestor Kirchner cuelga cuadros de los presidentes Argentinos faltantes", opt3: "Nestor Kirchner baja los cuadros para reenmarcarlos" },
-        { id: 5, tematica: "Abuela recuperando un nieto desaparecido en la dictadura", image: "/img/ruletaImagenes/tematica5.jpg", opt1: "Abuela recuperando un nieto desaparecido en la dictadura", opt2: "Dos personas festejando la vuelta a la democracia", opt3: "Madre y hijo con una foto por el dia de la madre" },
-        { id: 6, tematica: "Reclamo diplomatico y pacifico por las islas Malvinas", image: "/img/ruletaImagenes/tematica6.jpg", opt1: "Reclamo diplomatico y pacifico por las islas Malvinas", opt2: "Foto de las Islas Malvinas sacadas por un turista", opt3: "Foto del pacifico Argentino" },
-        { id: 7, tematica: "Primera marcha de las madres de plaza de mayo", image: "/img/ruletaImagenes/tematica7.jpg", opt1: "Primera marcha de las madres de plaza de mayo", opt2: "Madres cortando la calle por falta de luz", opt3: "Madres festejando la vuelta a la democracia" },
-        { id: 8, tematica: "Remodelacion de centros de detencion de la dictadura", image: "/img/ruletaImagenes/tematica8.jpg", opt1: "Remodelacion de centros de detencion de la dictadura", opt2: "Museo de arte de Buenos Aires", opt3: "Muestra de fotos en Berlin" },
+        { id: "d-1", tematica: "Juicio Juntas", image: "/img/ruletaImagenes/tematica1.jpg", opt1: "Juicio Juntas", opt2: "Diputados de la Nacion recibiendo distinción", opt3: "Juicio por las Malvinas" },
+        { id: "d-2", tematica: "Leyes Obedencia debida y punto", image: "/img/ruletaImagenes/tematica2.jpg", opt1: "Leyes Obedencia debida y punto", opt2: "Menem asume la presidencia", opt3: "Oficiales Armadas presentandose ante la nación" },
+        { id: "d-3", tematica: "Vuelta a la democracia", image: "/img/ruletaImagenes/tematica3.jpg", opt1: "Vuelta a la democracia", opt2: "Creacion de la nueva bandera Argentina", opt3: "Presidente de Estados Unidos visita la casa Rosada" },
+        { id: "d-4", tematica: "Nestor Kirchner baja los cuadros de la dictadura", image: "/img/ruletaImagenes/tematica4.jpg", opt1: "Nestor Kirchner baja los cuadros de la dictadura", opt2: "Nestor Kirchner cuelga cuadros de los presidentes Argentinos faltantes", opt3: "Nestor Kirchner baja los cuadros para reenmarcarlos" },
+        { id: "d-5", tematica: "Abuela recuperando un nieto desaparecido en la dictadura", image: "/img/ruletaImagenes/tematica5.jpg", opt1: "Abuela recuperando un nieto desaparecido en la dictadura", opt2: "Dos personas festejando la vuelta a la democracia", opt3: "Madre y hijo con una foto por el dia de la madre" },
+        { id: "d-6", tematica: "Reclamo diplomatico y pacifico por las islas Malvinas", image: "/img/ruletaImagenes/tematica6.jpg", opt1: "Reclamo diplomatico y pacifico por las islas Malvinas", opt2: "Foto de las Islas Malvinas sacadas por un turista", opt3: "Foto del pacifico Argentino" },
+        { id: "d-7", tematica: "Primera marcha de las madres de plaza de mayo", image: "/img/ruletaImagenes/tematica7.jpg", opt1: "Primera marcha de las madres de plaza de mayo", opt2: "Madres cortando la calle por falta de luz", opt3: "Madres festejando la vuelta a la democracia" },
+        { id: "d-8", tematica: "Remodelacion de centros de detencion de la dictadura", image: "/img/ruletaImagenes/tematica8.jpg", opt1: "Remodelacion de centros de detencion de la dictadura", opt2: "Museo de arte de Buenos Aires", opt3: "Muestra de fotos en Berlin" },
     ]
 
     const assignTimeById = (id) => {
@@ -64,51 +66,57 @@ const Ruleta = () => {
         return result;
     }
 
+
+    //arrow x(left) : 370, arrow y(top) : 186.5  
     const startRotation = () => {
-        setDisabledButton(true)
-
-        let num = Math.floor(Math.random() * data.length);
-
-        setDataForModal(data[num]);
-        const duration = assignTimeById(data[num].id)
+        setDisabledButton(false)
 
         setFrase({
             name: "circle start-rotate"
         })
 
+        const durationSetTimeOut1 = Math.floor(Math.random() * 10000) + 1
+
         setTimeout(() => {
             setFrase({
                 name: "circle start-rotate stop-rotate"
             })
-            const arrow = document.getElementById('flecha').getBoundingClientRect() //arrow x top:207 y:232 //  
 
-            var lists = document.querySelectorAll('li')
+            const innerWidth = window.innerWidth;
+            const innerHeight = window.innerHeight;
 
-            console.log(Math.floor(arrow.x),Math.floor(arrow.y))
+            const spinButton = document.getElementById('middle-button')
 
-            //Rangos de deteccion de la arrow con respecto a cada li
+            spinButton.classList.add("hide")
 
-            lists.forEach((li,index) => {
-            const elPos = li.getBoundingClientRect();
-            const rangeWidthStart = elPos.x
-            const rangeWidthFinish = elPos.x + elPos.width
-            const rangeHeightStart = elPos.y
-            const rangeHeightFinish = elPos.y + elPos.height
-            if(arrow.x > rangeWidthStart && arrow.x < rangeWidthFinish && arrow.y > rangeHeightStart && arrow.y < rangeHeightFinish && rangeHeightStart < 80 && rangeHeightStart > 50 ){
-                console.log("width",Math.floor(rangeWidthStart),Math.floor(rangeWidthFinish),"height",Math.floor(rangeHeightStart),Math.floor(rangeHeightFinish))
-                console.log(li)
-            }
+            const location = document.elementFromPoint(512, 324) //coordenadas basadas en un size de window width 1010x853 de height            
+
+
+            data.forEach((e, index) => {
+                if (e.id == location.id) {
+                    console.log(e.id)
+                    if (location.attributes.class.nodeValue == "text") {
+                        const elementLiId = location.parentNode.className
+
+                        var obj, matrix;
+                        obj = document.getElementById('circulo');
+                        matrix = getComputedStyle(obj).getPropertyValue('transform');
+                        console.log(qrDecompone(matrix))
+                    }
+                    setDataForModal(data[index])
+                }
             })
 
-            // const getTouchElement = document.elementFromPoint(arrow.x,)
-            // console.log(getTouchElement)
-        }, Math.floor(Math.random() * 10000) + 1)
+            // left: calc(50% - 0px);
+            // top: calc(50% - 200px);
+
+        }, durationSetTimeOut1)
 
         // setTimeout(() => {
         //     setOpenModalState(true)
-        // }, 5000)
-
-        num = 0;
+        //     const spinButton = document.getElementById('middle-button')
+        //     spinButton.classList.remove('hide')
+        // }, durationSetTimeOut1 + 1000)
 
     };
 
@@ -116,84 +124,86 @@ const Ruleta = () => {
         <div className="main-container">
             <div className="container">
                 <div id="flecha" className="arrow"></div>
-                <button className='spin-button' onClick={() => startRotation()} disabled={disabledButton}></button>
-                <ul className={frase.name}>
-                    <li id="li-1">
+                <div id="testPoint" style={{ width: 3, height: 3, backgroundColor: 'red', position: "absolute", zIndex: 1 }}></div>
+                <button className='spin-button' id="middle-button" onClick={() => startRotation()}></button>
+                <ul className={frase.name} id="circulo">
+                    <li id="li-1" className='list-1'>
                         <div className="text"
                             spellCheck="false"
-                           >
-                            <div className="img-container">
-                                <img src={data[0].image} alt="" style={{ height: "100%", width: "100%", borderRadius: "5px" }} />
+                            id="d-1"
+                        >
+                            <div className="img-container" id="d-1">
+                                <img src={data[0].image} alt="" style={{ height: "100%", width: "100%", borderRadius: "5px" }} id="d-1" />
                             </div>
                         </div>
                     </li>
-                    <li id="li-2">
+                    <li id="li-2" className='list-2'>
                         <div className="text"
                             spellCheck="false"
-                            
-                            >
-                            <div className="img-container">
-                                <img src={data[1].image} alt="" style={{ height: "100%", width: "100%", borderRadius: "5px" }} />
+                            id="d-2"
+                        >
+                            <div className="img-container" id="d-2">
+                                <img src={data[1].image} alt="" style={{ height: "100%", width: "100%", borderRadius: "5px" }} id="d-2" />
                             </div>
                         </div>
                     </li>
-                    <li id="li-3">
+                    <li id="li-3" className='list-3'>
                         <div className="text"
                             spellCheck="false"
-                            
-                           >
-                            <div className="img-container">
-                                <img src={data[2].image} alt="" style={{ height: "100%", width: "100%", borderRadius: "5px" }} />
+                            id="d-3"
+                        >
+                            <div className="img-container" id="d-3">
+                                <img src={data[2].image} alt="" style={{ height: "100%", width: "100%", borderRadius: "5px" }} id="d-3" />
                             </div>
                         </div>
                     </li>
-                    <li id="li-4">
+                    <li id="li-4" className='list-4'>
                         <div className="text"
                             spellCheck="false"
-                            
-                            >
-                            <div className="img-container">
-                                <img src={data[3].image} alt="" style={{ height: "100%", width: "100%", borderRadius: "5px" }} />
+                            id="d-4"
+                        >
+                            <div className="img-container" id="d-4">
+                                <img src={data[3].image} alt="" style={{ height: "100%", width: "100%", borderRadius: "5px" }} id="d-4" />
                             </div>
                         </div>
                     </li>
-                    <li id="li-5">
+                    <li id="li-5" className='list-5'>
                         <div className="text"
                             spellCheck="false"
-                            
-                            >
-                            <div className="img-container">
-                                <img src={data[4].image} alt="" style={{ height: "100%", width: "100%", borderRadius: "5px" }} />
+                            id="d-5"
+                        >
+                            <div className="img-container" id="d-5">
+                                <img src={data[4].image} alt="" style={{ height: "100%", width: "100%", borderRadius: "5px" }} id="d-5" />
                             </div>
                         </div>
                     </li>
-                    <li id="li-6">
+                    <li id="li-6" className='list-6'>
                         <div className="text"
                             spellCheck="false"
-                            
-                           >
-                            <div className="img-container">
-                                <img src={data[5].image} alt="" style={{ height: "100%", width: "100%", borderRadius: "5px" }} />
+                            id="d-6"
+                        >
+                            <div className="img-container" id="d-6">
+                                <img src={data[5].image} alt="" style={{ height: "100%", width: "100%", borderRadius: "5px" }} id="d-6" />
                             </div>
                         </div>
                     </li>
-                    <li id="li-7">
+                    <li id="li-7" className='list-7'>
                         <div className="text"
                             spellCheck="false"
-                           
-                            >
-                            <div className="img-container" >
-                                <img src={data[6].image} alt="" style={{ height: "100%", width: "100%", borderRadius: "5px" }} />
+                            id="d-7"
+                        >
+                            <div className="img-container" id="d-7">
+                                <img src={data[6].image} alt="" style={{ height: "100%", width: "100%", borderRadius: "5px" }} id="d-7" />
                             </div>
                         </div>
                     </li>
-                    <li id="li-8">
+                    <li id="li-8" className='list-8'>
                         <div className="text"
                             spellCheck="false"
-                            
-                            >
-                            <div className="img-container">
-                                <img src={data[7].image} alt="" style={{ height: "100%", width: "100%", borderRadius: "5px" }} />
+                            id="d-8"
+                        >
+                            <div className="img-container" id="d-8">
+                                <img src={data[7].image} alt="" style={{ height: "100%", width: "100%", borderRadius: "5px" }} id="d-8" />
                             </div>
                         </div>
                     </li>
