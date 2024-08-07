6064831d9dd92f1c037afb42d2828e878164a0fd
Tomas Tosello <tomitosello@gmail.com>
2024-08-07 17:40:01 -0300

    Modificado tamaño pantalla grande productos

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -10,7 +10,7 @@ const Home = () => {
   return (
     <div className='container-main-home' style={{width:'100%',display:'flex',justifyContent:'center',alignItems:'center'}}>
       <div className='container-main-juegos'>
-          <img className='imagen-parque' src="/logo-cormecoritos.png" alt="" srcset=""/>
+          <img className='imagen-parque' src="/logo-cormecoritos.png" alt="" />
           <div className='btns-juegos'>
             <Button className="btn-drag" variant="contained" onClick={() => navigate('/dragdrop')}>Juego de Imágenes <img style={{paddingLeft:'20px'}} src="/imagen-logo.svg" alt="" /></Button>
             <Button className="btn-memory" variant="contained" onClick={() => navigate('/memo')}>Juego de Memoria <img style={{paddingLeft:'20px'}} src="/memory-logo.svg" alt="" /></Button>
